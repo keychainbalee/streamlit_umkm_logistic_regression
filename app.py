1089788bb23bacfd2c882b978b92baa1ce7a19c0
keychainbalee <muhammad.iqbal.sap@gmail.com>
2026-06-13 11:09:22 +0700

    fix : sidebar

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
+import streamlit as st
+
 from views.dashboard import show_dashboard
 from views.comparison import show_comparison
 from views.prediction import show_prediction
 
-import streamlit as st
 
-page = st.sidebar.selectbox(
-    "Menu",
-    [
-        "Dashboard",
-        "Prediksi Kesuksesan UMKM",
-        "Perbandingan Dataset"
-    ]
-)
+if "page" not in st.session_state:
+    st.session_state.page = "Dashboard"
+
+st.sidebar.title("UMKM Insight Dashboard")
+
+if st.sidebar.button("Dashboard", use_container_width=True):
+    st.session_state.page = "Dashboard"
+
+if st.sidebar.button("Prediksi Data", use_container_width=True):
+    st.session_state.page = "Prediksi Data"
+    
+if st.sidebar.button("Perbandingan Dataset", use_container_width=True):
+    st.session_state.page = "Perbandingan Dataset"
+
+
+
+page = st.session_state.page
 
 if page == "Dashboard":
     show_dashboard()
-elif page == "Prediksi Kesuksesan UMKM":
+
+elif page == "Prediksi Data":
     show_prediction()
-else:
+    
+elif page == "Perbandingan Dataset":
     show_comparison()
